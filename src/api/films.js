589a8e0e6075ee9e139226ace756eb1dc6d71322
Juanma <juanmanuelloram@gmail.com>
2022-05-02 11:37:08 +0200

    feat() added catch to api added animation improvement

--- src/api/films.js
+++ src/api/films.js
@@ -1,13 +1,13 @@
 function searchFilms() {
-  return fetch("https://api.tvmaze.com/search/shows?q=girls").then((resp) =>
-    resp.json()
-  );
+  return fetch("https://api.tvmaze.com/search/shows?q=girls")
+    .then((resp) => resp.json())
+    .catch((e) => e.getMessage());
 }
 
 function getFilm(filmId) {
-  return fetch(`https://api.tvmaze.com/shows/${filmId}`).then((resp) =>
-    resp.json()
-  );
+  return fetch(`https://api.tvmaze.com/shows/${filmId}`)
+    .then((resp) => resp.json())
+    .catch((e) => e.getMessage());
 }
 
 export { searchFilms, getFilm };
